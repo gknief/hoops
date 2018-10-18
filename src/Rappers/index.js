7c51f4b54e93aa81fe9047a4a5c51cb0782d41e0
Grant Knief <gknief@mac.com>
2018-10-17 21:54:32 -0400

    mixtape rendering with video and rapper/added rappers

--- src/Rappers/index.js
+++ src/Rappers/index.js
@@ -13,7 +13,7 @@ export default class Rappers extends Component {
             <div className="rappers-container">
                 <h1 className="rappers-header">Rappers</h1>
                 <h2>Which rapper do you want on your mixtape?</h2>
-                <select name="rapper" value={this.props.rapper} onChange={this.props.onPickRapper}>
+                <select name="rapper" onChange={this.props.onPickRapper}>
                 {this.props.rappers.map(rapper => {
                     return <option key={rapper.id} value={rapper.name}>{rapper.name}</option>
                 })}
