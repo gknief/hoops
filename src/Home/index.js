e12c76206cc2ee59ac935e11b1689679012f15c4
Grant Knief <gknief@mac.com>
2018-10-22 01:12:50 -0400

    completed project

--- src/Home/index.js
+++ src/Home/index.js
@@ -18,11 +18,15 @@ export default class Home extends Component {
         <div className="home-container">
           <img className="logo" src="../images/logo.png" />
           <h2 className="slogan">Your favorite ballers paired with your favorite beats.</h2>
+          <Link to='/PlayersRappers'><button className='get-started'>GET STARTED</button></Link>
           <div className="mission-statement-container">
+          <div className="home-image-container">
+          <img src="../images/lebron.png" className="lebron" />
+          <img src="../images/drake.png" className="drake" />
+          </div>
           <article className="mission-statement">I have always loved listening to hip-hop music and watching highlights of my favorite basketball players. But I wanted to be able to do both at the same time without YouTube pausing my music. With Mixtape, you can watch highlights of any player while listening to any song you want.</article>
           <h3> - Grant Knief, founder of Mixtape</h3>
           </div>
-          <Link to='/PlayersRappers'><button className='get-started'>GET STARTED</button></Link>
             {/* <button className='players-button'><Link to='/players'>Players</Link></button>
             <button className='rappers-button'><Link to='/rappers'>Rappers</Link></button> */}
         <iframe width="0" height="0" src={`https://www.youtube.com/embed/Lv07rGgIOMc?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1&amp;mute=0`} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
